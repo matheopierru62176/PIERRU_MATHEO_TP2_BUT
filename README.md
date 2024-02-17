bc914264ca00942b94f023e31bf87c1d418cda50
PIERRU Matheo <matheo.pierru@etu.univ.littoral.fr>
2024-02-17 11:09:24 +0100

    ajout des fichiers

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 COMPTE RENDU DU TP2 : 
 
+Réaliser : 
+
+ - Créer un utilisateur
+ - Ajouter un item au registre
+ - Créer une watchlist pour un utilisateur
+
+    Ajouter un item dans une watchlist ( pas sur que sa fonctionne  )
+
+
+
+Non réaliser : 
+
+Modifier le statut d'un item dans une watchlist
+Afficher les items du registre (avec possibilité de filtrer)
+Récupérer la liste des utilisateurs
+Récupérer la liste des watchlists d'un utilisateur
+Récupérer le contenu d'une watchlist
--- src/controllers/watchlist.js
+++ src/controllers/watchlist.js
@@ -25,9 +25,33 @@ async function createWatchlist(req, res, next) {
 }
 
 
+async function ajoutItem(req, res, next) {
+  try{
+    // Vérifier que l'utilisateur existe
+    const userId = req.body.utilisateurId;
+    // console.log(userId);
+
+    const user = await findOne("users", {"id" : (userId)});
+    if (user) {
+
+    console.log(req.body);
+    const result = await insertOne(collection, req.body);
+    console.log(`Nouvelle watchlist créee avec l'id ${result.insertedId}`)
+    return res.status(201).send(result);
+    } else {
+      return res.status(404).send("Utilisateur non trouvé");
+    }
+  } catch (e) {
+    console.log(e);
+    return next(e);
+  }
+}
+
+
 
 
 
 module.exports = {
   createWatchlist,
+  ajoutItem
 };
